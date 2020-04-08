76fd29e4633c8767f3d36ba54aba3e72052ef9ae
Bea Cobo <cobo@ifca.unican.es>
2020-04-08 17:19:14 +0200

    Input parameters default values updated to those most commonly used

--- SIRENAthreading/versionSIRENA.h
+++ SIRENAthreading/versionSIRENA.h
@@ -22,11 +22,11 @@
 //   CANTABRIA (CSIC-UC) with funding from the Spanish Ministry of Science and 
 //   Innovation (MICINN)
 //
-//   DATE: 2020/04/08, 15:06:29
+//   DATE: 2020/04/08, 17:19:14
 
 #ifndef SIRENA_VERSION_H
 #define SIRENA_VERSION_H
 
-#define SIRENA_VERSION "2.1.2"
+#define SIRENA_VERSION "2.1.3"
 
 #endif
