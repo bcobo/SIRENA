8dce87b7dc93b1a0c5fd475f37efdb361c2734ea
Bea Cobo <cobo@ifca.unican.es>
2020-05-28 17:28:47 +0200

    SIRENA update (v.3.1.1) SIRENA update how getting the sampling rate and keywords (no matter the number of HDUs)
    - TESRECONSTRUCTION: - Sampling rate read from the XML file (and check it with the value in HISTORY)
    		     - No matter how many HDUs in the input FITS file (it checks if 'RECORDS' or 'TESRECORDS')
    - GENNOISESPEC: - New input parameter 'samplingRate' (not defeault value hardcoded):
                    	- Not provided => Read from the input FITS file (HISTORY)
                            - Provided => Check it with the value in HISTORY
                    - No matter how many HDUs in the input FITS file (it checks if 'RECORDS' or 'TESRECORDS')

--- SIRENAthreading/versionSIRENA.h
+++ SIRENAthreading/versionSIRENA.h
@@ -22,11 +22,11 @@
 //   CANTABRIA (CSIC-UC) with funding from the Spanish Ministry of Science and 
 //   Innovation (MICINN)
 //
-//   DATE: 2020/05/22, 17:58:20
+//   DATE: 2020/05/28, 17:28:47
 
 #ifndef SIRENA_VERSION_H
 #define SIRENA_VERSION_H
 
-#define SIRENA_VERSION "3.1.0"
+#define SIRENA_VERSION "3.1.1"
 
 #endif
