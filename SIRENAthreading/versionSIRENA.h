df615cfc937bd841ee492229e302394db05e7c67
Bea Cobo <cobo@ifca.unican.es>
2020-07-31 10:50:38 +0200

    SIRENA update (v.3.4.9): GENNOISESPEC runs with only one noise interval (solved bug in medianKappaClipping)

--- SIRENAthreading/versionSIRENA.h
+++ SIRENAthreading/versionSIRENA.h
@@ -22,11 +22,11 @@
 //   CANTABRIA (CSIC-UC) with funding from the Spanish Ministry of Science and 
 //   Innovation (MICINN)
 //
-//   DATE: 2020/07/27, 20:47:01
+//   DATE: 2020/07/31, 10:50:38
 
 #ifndef SIRENA_VERSION_H
 #define SIRENA_VERSION_H
 
-#define SIRENA_VERSION "3.4.8"
+#define SIRENA_VERSION "3.4.9"
 
 #endif
